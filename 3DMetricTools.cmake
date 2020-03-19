c90e0720119999c39ef8a97c1a2ec8382ee0c8e6
Jean-Christophe Fillion-Robin <jchris.fillionr@kitware.com>
2020-03-19 00:18:20 -0400

    COMP: Generate (Extension)Config.cmake
    This commit allows an other extension to import this extension targets.

--- 3DMetricTools.cmake
+++ 3DMetricTools.cmake
@@ -106,5 +106,6 @@ endif()
 #-----------------------------------------------------------------------------
 if( ${EXTENSION_NAME}_BUILD_SLICER_EXTENSION )
   set(CPACK_INSTALL_CMAKE_PROJECTS "${CPACK_INSTALL_CMAKE_PROJECTS};${CMAKE_BINARY_DIR};${EXTENSION_NAME};ALL;/")
+  include(${Slicer_EXTENSION_GENERATE_CONFIG})
   include(${Slicer_EXTENSION_CPACK})
 endif()
